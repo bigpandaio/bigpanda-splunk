1011b4d98b5df7e391f3868ea1091e2c2c9ebf0a
erikzaadi <erik.zaadi@gmail.com>
2014-09-07 18:01:25 +0300

    new runner (prevent custom splunk env injection)

--- bigpanda_splunk/configure.py
+++ bigpanda_splunk/configure.py
@@ -4,8 +4,21 @@ from bigpanda_splunk import \
         LOG, write_config, setup_logging, get_path
 from bigpanda_splunk.runner import send_test_alert
 
+import glob
+import grp
 import os
+import pwd
 import sys
+
+SPLUNK_USER_NAME = "splunk"
+
+GID, UID = None, None
+
+try:
+    GID = grp.getgrnam(SPLUNK_USER_NAME).gr_gid
+    UID = pwd.getpwnam(SPLUNK_USER_NAME).pw_uid
+except:
+    pass
 
 def configure(runner_path):
     """
@@ -16,14 +29,20 @@ def configure(runner_path):
         sys.exit("BigPanda Splunk: Splunk installation not found!")
 
     try:
-        link_target = get_path(SCRIPT_NAME)
-        if os.path.exists(link_target):
-            os.unlink(link_target)
-        os.symlink(
-            os.path.join(runner_path, SCRIPT_NAME), link_target)
+        bp_shell_path = get_path(SCRIPT_NAME)
+        if os.path.exists(bp_shell_path):
+            os.unlink(bp_shell_path)
+        bp_shell_content = """#!/bin/bash
+PYTHONPATH= LD_LIBRARY_PATH= %s %s $@[]
+""" % (sys.executable, os.path.join(runner_path, SCRIPT_NAME))
+        with open(bp_shell_path, "w") as shell_file:
+            shell_file.write(bp_shell_content)
         write_config(token, app_key)
         setup_logging()
         LOG.info("Testing permissions")
+        if GID and UID:
+            for bp_file in glob.glob(get_path('') + "/bigpanda*"):
+                os.chown(bp_file, UID, GID)
     except Exception as error:
         sys.exit(
             "BigPanda Splunk: Invalid permissions detected: %s" % str(error))
