502377df7ed99b3510c7b28191f82d8d79c08cdb
erikzaadi <erik.zaadi@gmail.com>
2014-09-22 17:11:58 +0300

    TESTZ EVERYWHEREZ

--- bigpanda_splunk/configure.py
+++ bigpanda_splunk/configure.py
@@ -11,40 +11,24 @@ import pwd
 import sys
 
 SPLUNK_USER_NAME = "splunk"
+PRINT_PREFIX = "BigPanda Splunk"
 
-GID, UID = None, None
 
-try:
-    GID = grp.getgrnam(SPLUNK_USER_NAME).gr_gid
-    UID = pwd.getpwnam(SPLUNK_USER_NAME).pw_uid
-except:
-    pass
-
-def configure(runner_path):
+def configure(runner_path, args):
     """
     Configure BigPanda Splunk Provider
     """
-    token, app_key = sys.argv[1], sys.argv[2]
-    if not os.path.exists(SPLUNK_HOME):
-        sys.exit("BigPanda Splunk: Splunk installation not found!")
-
+    error = validate_args(runner_path, args)
+    if error:
+        return sys.exit(error)
+    token, app_key = args[1], args[2]
     try:
-        bp_shell_path = get_path(SCRIPT_NAME)
-        if os.path.exists(bp_shell_path):
-            os.unlink(bp_shell_path)
-        bp_shell_content = """#!/bin/bash
-PYTHONPATH= LD_LIBRARY_PATH= %s %s "$@"
-""" % (sys.executable, os.path.join(runner_path, SCRIPT_NAME))
-        with open(bp_shell_path, "w") as shell_file:
-            shell_file.write(bp_shell_content)
+        write_shell_file(runner_path)
         write_config(token, app_key)
-        setup_logging()
-        LOG.info("Testing permissions")
-        if GID and UID:
-            for bp_file in glob.glob(get_path('') + "/bigpanda*"):
-                os.chown(bp_file, UID, GID)
+        verify_logging_permissions()
+        chown_bigpanda_files()
     except Exception as error:
-        sys.exit(
+        return sys.exit(
             "BigPanda Splunk: Invalid permissions detected: %s" % str(error))
 
     error = send_test_alert()
@@ -52,3 +36,40 @@ PYTHONPATH= LD_LIBRARY_PATH= %s %s "$@"
         sys.exit(error)
     print 'BigPanda Splunk Integration Configured.\n\
 Your should see a test alert at your BigPanda dashboard...'
+
+def write_shell_file(runner_path):
+    bp_shell_path = get_path(SCRIPT_NAME)
+    if os.path.exists(bp_shell_path):
+        os.unlink(bp_shell_path)
+    bp_shell_content = """#!/bin/bash
+PYTHONPATH= LD_LIBRARY_PATH= %s %s "$@"
+""" % (sys.executable, os.path.join(os.path.dirname(runner_path), SCRIPT_NAME))
+    with open(bp_shell_path, "w") as shell_file:
+        shell_file.write(bp_shell_content)
+
+
+def verify_logging_permissions():
+    setup_logging()
+    LOG.info("Testing permissions")
+
+
+def chown_bigpanda_files():
+    try:
+        GID = grp.getgrnam(SPLUNK_USER_NAME).gr_gid
+        UID = pwd.getpwnam(SPLUNK_USER_NAME).pw_uid
+    except:
+        pass
+    if GID and UID:
+        for bp_file in glob.glob(get_path('') + "/bigpanda*"):
+            os.chown(bp_file, UID, GID)
+
+
+def validate_args(runner_path, args):
+    if not args or len(args) != 3:
+        return "%s\nUsage: %s TOKEN APP_KEY" % (
+            PRINT_PREFIX,
+            os.path.basename(runner_path)
+            )
+                
+    if not os.path.exists(SPLUNK_HOME):
+        return "%s: Splunk installation not found!" % PRINT_PREFIX
--- tests/test_configure.py
+++ tests/test_configure.py
@@ -2,9 +2,8 @@ import pytest
 import sys
 from mock import patch, MagicMock, mock_open
 
-from bigpanda_splunk.configure import configure
+from bigpanda_splunk.configure import configure, write_shell_file, chown_bigpanda_files, verify_logging_permissions
 from tests import WTFException
-
 
 
 class TestConfigure(object):
@@ -20,14 +19,40 @@ class TestConfigure(object):
 
         sys_exit_mock.assert_called_once_with("BigPanda Splunk: Splunk installation not found!")
 
-    @patch("bigpanda_splunk.configure.write_config")
     @patch("os.unlink")
-    @patch("sys.exit")
     @patch("os.path.exists", return_value=True)
-    def test_configure_should_write_shell_wrapper_file(self, exists_mock, sys_exit_mock, unlink_mock, write_config_mock):
+    def test_write_shell_file(self, exists_mock, unlink_mock):
         open_mock = mock_open()
-        write_config_mock.side_effect = WTFException
         with patch("bigpanda_splunk.configure.open", open_mock, create=True):
-            configure("/bla/runner", ["", "token", "app_key"])
-        print "Call Args?", write_config_mock.call_args
+            write_shell_file("/bla/runner")
+
         open_mock.assert_called_once_with("/opt/splunk/bin/scripts/bigpanda-splunk", "w")
+
+
+    @patch("bigpanda_splunk.configure.validate_args", return_value=None)
+    @patch("sys.exit")
+    @patch("bigpanda_splunk.configure.write_shell_file")
+    def test_configure_should_write_shell_wrapper_file(self, write_shell_file_mock, sys_exit_mock, validate_args_mock):
+        write_shell_file_mock.side_effect = WTFException("AAA")
+        configure("/bla/runner", ["", "token", "app_key"])
+        write_shell_file_mock.assert_called_once_with("/bla/runner")
+
+        sys_exit_mock.assert_called_once_with("BigPanda Splunk: Invalid permissions detected: AAA")
+
+    @patch("bigpanda_splunk.configure.LOG")
+    @patch("bigpanda_splunk.configure.setup_logging")
+    def test_verify_logging_permissions(self, setup_logging_mock, log_mock):
+        verify_logging_permissions()
+        setup_logging_mock.assert_called_once()
+        log_mock.info.assert_called_once_with("Testing permissions")
+
+    @patch("bigpanda_splunk.configure.grp")
+    @patch("bigpanda_splunk.configure.pwd")
+    @patch("bigpanda_splunk.configure.glob")
+    @patch("bigpanda_splunk.configure.os")
+    def test_chown_bigpanda_files(self, os_mock, glob_mock, pwd_mock, grp_mock):
+        grp_mock.getgrnam.return_value.gr_gid = "GIDz"
+        pwd_mock.getpwnam.return_value.pw_uid = "UIDz"
+        glob_mock.glob.return_value = ["f"]
+        chown_bigpanda_files()
+        os_mock.chown.assert_called_once_with("f", "UIDz", "GIDz")
