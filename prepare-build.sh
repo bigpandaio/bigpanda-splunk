6dc49590bbe0aec1aabfcdd0333ef1263c5af35b
Shahar Kedar <shahar@gmail.com>
2014-11-04 18:54:15 +0200

    always use git describe --always

--- prepare-build.sh
+++ prepare-build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 TAG=${TRAVIS_TAG:-"unknown"}
 if [[ "${TAG}" = "unknown" ]]; then
-    TAG=`git describe`
-    if [[ "$?" != "0" ]]; then
-        TAG=`git describe --always`
-    fi
+    TAG=`git describe --always`
 fi
 echo ${TAG} | cut -c 1- > VERSION
